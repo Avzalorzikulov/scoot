b54c63518356ed51a8367316c7e37c0e60812d44
HP <avzalorzikulov39@gmail.com>
2024-01-19 08:09:11 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+elBtn = document.querySelector('.site-btn');
+elContent = document.querySelector('.site-item-bottom');
+
+elBtn.addEventListener('click', function() {
+    elContent.classList.toggle('open');
+    elBtn.classList.toggle('rotate')
+})
+
+elBtn2 = document.querySelector('.site-btn2');
+elContent2 = document.querySelector('.site-item-bottom2');
+
+elBtn2.addEventListener('click', function() {
+    elContent2.classList.toggle('open2');
+    elBtn2.classList.toggle('rotate2')
+})
+elBtn3 = document.querySelector('.site-btn3');
+elContent3 = document.querySelector('.site-item-bottom3');
+
+elBtn3.addEventListener('click', function() {
+    elContent3.classList.toggle('open3');
+    elBtn3.classList.toggle('rotate3')
+})
+
+
+elBtn4 = document.querySelector('.site-btn4');
+elContent4 = document.querySelector('.site-item-bottom4');
+
+elBtn4.addEventListener('click', function() {
+    elContent4.classList.toggle('open4');
+    elBtn4.classList.toggle('rotate4')
+})
+elBtn5 = document.querySelector('.site-btn5');
+elContent5 = document.querySelector('.site-item-bottom5');
+
+elBtn5.addEventListener('click', function() {
+    elContent5.classList.toggle('open5');
+    elBtn5.classList.toggle('rotate5')
+})
+elBtn6 = document.querySelector('.site-btn6');
+elContent6 = document.querySelector('.site-item-bottom6');
+
+elBtn6.addEventListener('click', function() {
+    elContent6.classList.toggle('open6');
+    elBtn6.classList.toggle('rotate6')
+})
